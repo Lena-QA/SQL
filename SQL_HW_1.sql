9fc595edf84788d70d25949cc9ff959caada1966
Elena <cherkovskaya.lena@gmail.com>
2022-06-22 21:59:37 +0300

    SQL_HW_1.sql

--- SQL_HW_1.sql
+++ SQL_HW_1.sql
@@ -124,7 +124,7 @@ select Count(id) from students;
 select id, name, created_on from students
 order by created_on asc;
 
-Вывести id пользователя, имя, дату создания пользователя. 
+32. Вывести id пользователя, имя, дату создания пользователя. 
 Отсортировать по порядку убывания даты добавления пользователя.
 
 select id, name, created_on from students 
